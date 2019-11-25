50719d4f88f64fe2733b900fe2c554abaddca8b4
rjcpc <52098528+rjcpc@users.noreply.github.com>
2019-11-25 14:45:49 +0530

    Add files via upload

---
+++ dotnet/fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonacci_series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1=0,n2=1,n3,i,number;
+            Console.Write("enter the number of elements:");
+            number = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+
+            }
+
+
+        }
+    }
+}
