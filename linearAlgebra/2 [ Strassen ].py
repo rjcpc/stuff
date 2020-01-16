d923f0886f8b4086415a81593b3e152db19c95d4
Shyamkumar Yadav <46707211+shyamkumaryadav@users.noreply.github.com>
2020-01-16 18:37:58 +0530

    Update 2 [ Strassen ].py

--- linearAlgebra/2 [ Strassen ].py
+++ linearAlgebra/2 [ Strassen ].py
@@ -2,33 +2,28 @@
 
 x=[[0,2],[0,1]]
 print("matrix x is")
+
 for i in range(len(x)):
     print("\t",x[i])
+
 y=[[0,0],[3,4]]
 print("matrix y is")
+
 for j in range(len(y)):
     print("\t",y[j])
 
 def st(a,b):
     s=[b[0][1]-b[1][1],
-
        a[0][0]+a[0][1],
-
        a[1][0]+a[1][1],
-
        b[1][0]+b[0][0],
-
        a[0][0]+a[1][1],
-
        b[0][0]+b[1][0],
-
        a[0][1]-a[1][1],
-
        b[1][0]+b[1][1],
-
        a[0][0]-a[1][0],
-
-       b[0][0]+b[0][1]]
+       b[0][0]+b[0][1]
+      ]
 
     p=[a[0][0]*s[0],
        s[1]*b[1][1],
@@ -37,12 +32,12 @@ def st(a,b):
        s[4]*s[5],
        s[6]*s[7],
        s[8]*s[9]]
-    c=[
-        [p[4]+p[3]-p[1]-p[5]],
+    c=[[p[4]+p[3]-p[1]-p[5]],
         [p[0]+p[1]],
         [p[2]+p[3]],
         [p[4]+p[0]-p[2]-p[6]]
-        ]
+       ]
+
     print("strassens matrix multiplication of x and y")
     print(s)
     print(p)
